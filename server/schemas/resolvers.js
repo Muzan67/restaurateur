cf12d5974509e2e9091d97f006f84d77f6e362b8
lotus <daniel.younghwan.lee@gmail.com>
2022-09-09 16:17:27 -0400

    getting resolvers to work

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -13,14 +13,28 @@ const resolvers = {
             return await Resource.findById({ _id })
         },
 
+        users: async () => {
+            return User.find()
+            .select('-__v -password');
+        },
+
         user: async (parent, args, context) => {
             if (context.user) {
-                const userData = await User.findOne({ _id: context.user._id })
+                const userData = await User.findById({ _id: context.user._id })
                 .select('-__v -password');
 
                 return userData;
             }
             throw new AuthenticationError('Not logged in');
+        }
+    },
+
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+
+            return { token, user };
         }
     }
 }
