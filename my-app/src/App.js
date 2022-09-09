327deb61009fb20fbfd1ece36da99c03f83f0d6d
Alejandro Cortez <102841726+Muzan67@users.noreply.github.com>
2022-09-08 23:38:10 -0400

    created react and files

---
+++ my-app/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+// import Header from "./components/Header";
+// import Footer from "./components/Footer";
+// import "./App.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import Contact from './components/Contact';
+
+const App = () => {
+  return (
+    <>
+      <div>
+        {/* <Header />
+      </div>
+      <div>
+        <Footer /> */}
+      </div>
+    </>
+  );
+};
+export default App;
---
+++ my-app/src/utils/helpers.js
@@ -0,0 +1,15 @@
+// email validation
+export function validateEmail(email) {
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+// password validation
+export function checkPassword(input) {
+  const passw = /^[A-Za-z]\w{7,14}$/;
+  if (input.match(passw)) {
+    return true;
+  }
+  return false;
+}
