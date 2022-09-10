a02544637d0bf3e9eb0fc44f43fc8c4efa8c5b59
Alejandro Cortez <102841726+Muzan67@users.noreply.github.com>
2022-09-10 19:09:22 -0400

    updated header and navigation

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,17 @@
 import React from "react";
-import About from "./components/About"
+import Homepage from "./components/Homepage"
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 // import Navigation from "./components/Navigation";
 // import Footer from "./components/Footer";
 // import "./index.css";
-// import "bootstrap/dist/css/bootstrap.min.css";
 // import Contact from './components/Contact';
 
 const App = () => {
   return (
     
       <div>
-        <About />
+        <Homepage />
       </div>
     
   
