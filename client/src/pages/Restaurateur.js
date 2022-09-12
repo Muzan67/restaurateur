dce15926f902ef40cefc66839c91c3bc894c344e
Alejandro Cortez <102841726+Muzan67@users.noreply.github.com>
2022-09-12 12:19:36 -0400

    updated pages

---
+++ client/src/pages/Restaurateur.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Col } from "react-bootstrap";
+
+function Restaurateur() {
+  return (
+    <section className="">
+      <div className="row justify-content-center" id="about-container">
+        <Col lg={6} md={12}>
+  <div className="name" style={{ textAlign: "center" }} >
+    Welcome Back! 
+  </div>
+    </Col>
+    </div>
+  </section>
+  );
+}
+
+export default Restaurateur;
