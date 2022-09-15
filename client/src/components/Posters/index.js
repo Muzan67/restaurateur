75f46b09aa205c1fec2061443153434f13bef0cd
Andrew Lee <alee68@fordham.edu>
2022-09-15 19:48:10 -0400

    fixed icons position to be centered at all times

--- client/src/components/Posters/index.js
+++ client/src/components/Posters/index.js
@@ -3,72 +3,75 @@ import React from "react";
 import posters from "../../posters.json";
 
 function PostersCard(props) {
-  return (
-    <div className="card col mx-4">
-      <div className="img-container" height="100px" >
-        <img alt={props.name} src={props.image} height="100%"/>
-      </div>
-      <div className="content">
-        <ul>
-          <li>
-            <p className="project-title" style={{ textAlign: "center" }}>
-              {props.name}
-            </p>
-          </li>
-            <li className="pb-5 mb-5">
-              <p className="project-description">{props.description}</p>
-            </li>
-            <li id="card-icons">
-              <div className="project-icons" style={{ textAlign: "center" }}>
-                <a href={props.application}>
-                  <img
-                    src="https://img.icons8.com/cute-clipart/134/000000/terms-and-conditions.png"
-                    alt="documentation"
-                    id="port-icon"
-                  />
-                </a>
-                <a href={props.externallink}>
-                  <img
-                    src="https://img.icons8.com/cute-clipart/128/000000/external-link.png"
-                    alt="externallink"
-                    id="port-icon"
-                  />
-                </a>
-              </div>
-            </li>
-        </ul>
-      </div>
-    </div>
-  );
+	return (
+		<div className="card col mx-4">
+			<div className="img-container" height="100px">
+				<img alt={props.name} src={props.image} height="100%" />
+			</div>
+			<div className="content">
+				<ul>
+					<li>
+						<p className="project-title" style={{ textAlign: "center" }}>
+							{props.name}
+						</p>
+					</li>
+					<li className="pb-5 mb-5">
+						<p className="project-description">{props.description}</p>
+					</li>
+					<li id="card-icons">
+						<div className="project-icons" style={{ textAlign: "center" }}>
+							<a href={props.application}>
+								<img
+									src="https://img.icons8.com/cute-clipart/120/000000/terms-and-conditions.png"
+									alt="documentation"
+									id="port-icon"
+								/>
+							</a>
+							<a href={props.externallink}>
+								<img
+									src="https://img.icons8.com/cute-clipart/120/000000/external-link.png"
+									alt="externallink"
+									id="port-icon"
+								/>
+							</a>
+						</div>
+					</li>
+				</ul>
+			</div>
+		</div>
+	);
 }
 
 function Wrapper(props) {
-  return <div className="wrapper">{props.children}</div>;
+	return <div className="wrapper">{props.children}</div>;
 }
 
 function Posters() {
-  return (
-    <>
-      <div className="project">
-        <h2 className="contact mt-5 fw-bold bg-dark bg-opacity-75" style={{ textAlign: "center" }}>
-          Posters
-        </h2>
-      </div>
+	return (
+		<>
+			<div className="project">
+				<h2
+					className="contact mt-5 fw-bold bg-dark bg-opacity-75"
+					style={{ textAlign: "center" }}
+				>
+					Posters
+				</h2>
+			</div>
 
-      <Wrapper>
-        {posters.map((project) => (
-          <PostersCard
-            key={project.id}
-            name={project.name}
-            image={project.image}
-            application={project.application}
-            externallink={project.externallink}
-            description={project.description}
-          />
-        ))}
-      </Wrapper>
-    </>
-  );
+			<Wrapper>
+				{posters.map((project) => (
+					<PostersCard
+						key={project.id}
+						name={project.name}
+						image={project.image}
+						application={project.application}
+						externallink={project.externallink}
+						description={project.description}
+					/>
+				))}
+			</Wrapper>
+		</>
+	);
 }
 
 export default Posters;
